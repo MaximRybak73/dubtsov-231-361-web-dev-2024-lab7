94484d981f777d63b911dd5fc34cab9a5af2423c
MaximRybak73 <rybolovclub73@mail.ru>
2024-10-12 19:34:02 +0300

    lab7

---
+++ static/main.js
@@ -0,0 +1,177 @@
+// Функция priority позволяет получить 
+// значение приоритета для оператора.
+// Возможные операторы: +, -, *, /.
+
+function priority(operation) {
+    if (operation == '+' || operation == '-') {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+// Проверка, является ли строка str числом.
+function isNumeric(str) {
+    return /^\d+(.\d+){0,1}$/.test(str);
+}
+
+// Проверка, является ли строка str цифрой.
+function isDigit(str) {
+    return /^\d{1}$/.test(str);
+}
+
+// Проверка, является ли строка str оператором.
+function isOperation(str) {
+    return /^[\+\-\*\/]{1}$/.test(str);
+}
+
+// Функция tokenize принимает один аргумент -- строку
+// с арифметическим выражением и делит его на токены 
+// (числа, операторы, скобки). Возвращаемое значение --
+// массив токенов.
+
+function tokenize(str) {
+    let tokens = [];
+    let lastNumber = '';
+    for (char of str) {
+        if (isDigit(char) || char == '.') {
+            lastNumber += char;
+        } else {
+            if (lastNumber.length > 0) {
+                tokens.push(lastNumber);
+                lastNumber = '';
+            }
+        }
+        if (isOperation(char) || char == '(' || char == ')') {
+            tokens.push(char);
+        }
+    }
+    if (lastNumber.length > 0) {
+        tokens.push(lastNumber);
+    }
+    return tokens;
+}
+
+// Функция compile принимает один аргумент -- строку
+// с арифметическим выражением, записанным в инфиксной 
+// нотации, и преобразует это выражение в обратную 
+// польскую нотацию (ОПН). Возвращаемое значение -- 
+// результат преобразования в виде строки, в которой 
+// операторы и операнды отделены друг от друга пробелами. 
+// Выражение может включать действительные числа, операторы 
+// +, -, *, /, а также скобки. Все операторы бинарны и левоассоциативны.
+// Функция реализует алгоритм сортировочной станции 
+// (https://ru.wikipedia.org/wiki/Алгоритм_сортировочной_станции).
+
+function compile(str) {
+    let out = [];
+    let stack = [];
+    for (token of tokenize(str)) { //разбиваем строку на токены, далее создается стек для операторов и 
+                                    //массив для выходного выражения
+                                    //затем преобр-ние в опн
+        if (isNumeric(token)) {
+            out.push(token);
+        } else if (isOperation(token)) {
+            while (stack.length > 0 && isOperation(stack[stack.length - 1]) && priority(stack[stack.length - 1]) >= priority(token)) {
+                out.push(stack.pop());
+            }
+            stack.push(token);
+        } else if (token == '(') {
+            stack.push(token);
+        } else if (token == ')') {
+            while (stack.length > 0 && stack[stack.length - 1] != '(') {
+                out.push(stack.pop());
+            }
+            stack.pop();
+        }
+    }
+    while (stack.length > 0) {
+        out.push(stack.pop());
+    }
+    return out.join(' ');
+}
+
+// Функция evaluate принимает один аргумент -- строку 
+// с арифметическим выражением, записанным в обратной 
+// польской нотации. Возвращаемое значение -- результат 
+// вычисления выражения. Выражение может включать 
+// действительные числа и операторы +, -, *, /.
+// Вам нужно реализовать эту функцию
+// (https://ru.wikipedia.org/wiki/Обратная_польская_запись#Вычисления_на_стеке).
+
+function evaluate(str) {
+    let stack = [];
+    let tokens = str.split(' '); //разделить строку на токены
+
+    for (let token of tokens) {
+        if (isNumeric(token)) {
+            stack.push(parseFloat(token)); // Если число, поместить в стек
+        } else if (isOperation(token)) { //если встречается оператор, 
+                                         //из стека извлекаются два числа, и выполняется операция
+            let b = stack.pop();
+            let a = stack.pop();
+            switch (token) {
+                case '+':
+                    stack.push(a + b);
+                    break;
+                case '-':
+                    stack.push(a - b);
+                    break;
+                case '*':
+                    stack.push(a * b);
+                    break;
+                case '/':
+                    stack.push(a / b);
+                    break;
+            }
+        }
+    }
+    return stack.pop().toFixed(2);
+    //после завершения работы в стеке остается один элемент — результат вычисления
+}
+
+// Функция clickHandler предназначена для обработки 
+// событий клика по кнопкам калькулятора. 
+// По нажатию на кнопки с классами digit, operation и bracket
+// на экране (элемент с классом screen) должны появляться 
+// соответствующие нажатой кнопке символы.
+// По нажатию на кнопку с классом clear содержимое экрана 
+// должно очищаться.
+// По нажатию на кнопку с классом result на экране 
+// должен появиться результат вычисления введённого выражения 
+// с точностью до двух знаков после десятичного разделителя (точки).
+// Реализуйте эту функцию. Воспользуйтесь механизмом делегирования 
+// событий (https://learn.javascript.ru/event-delegation), чтобы 
+// не назначать обработчик для каждой кнопки в отдельности.
+
+function clickHandler(event) {
+    const screen = document.querySelector('.screen span');// переменная screen будет ссылаться на элемент span, 
+                                                          //внутри которого будет отображаться выражение
+    const button = event.target; //определить какая кнопка была нажата
+
+    if (button.classList.contains('digit') || button.classList.contains('operation') || button.classList.contains('bracket')) {
+        screen.textContent += button.textContent;
+    } else if (button.classList.contains('clear')) {
+        screen.textContent = ''; //очистить экран
+    } else if (button.classList.contains('result')) {
+        const expression = screen.textContent;
+
+        // Проверка, что строка не пустая
+        if (expression.trim() === '') {
+            screen.textContent = 'Ошибка'; // при пустом вводе
+            return;
+        }
+
+        try {
+            const rpn = compile(expression);// в польскую нотацию
+            const result = evaluate(rpn); 
+            screen.textContent = result; //отобразить на экране
+        } catch (error) {
+            screen.textContent = 'Ошибка'; 
+        }
+    }
+}
+
+window.onload = function () {
+    document.querySelector('.buttons').addEventListener('click', clickHandler); //добавление обработчика события нажатия мыши
+}
